cab54686ce6986334ff3b7e2a1d6c0d2d8869d3b
misuto83 <virgile.bessiere@gmail.com>
2017-09-19 23:19:21 +0200

    rédaction model article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,17 @@
 class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to root_path
+  end
+
 end
